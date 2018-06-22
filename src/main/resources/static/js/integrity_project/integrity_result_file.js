46fb46b56b1de3a838e6f45d74245e6d5be7456a
161250005 <1046277348@qq.com>
2018-06-22 14:06:45 +0800

    commit

--- src/main/resources/static/js/integrity_project/integrity_result_file.js
+++ src/main/resources/static/js/integrity_project/integrity_result_file.js
@@ -4,22 +4,48 @@ var urls=new Array();
 
 $(document).ready(function () {
 var pid=localStorage.getItem("result_pid");
+var current_type=localStorage.getItem("view_result_type");
+if(current_type=="worker_result"){
+    var uid=localStorage.getItem("result_uid");
     $.ajax({
-        type : 'POST',
-        url : '/project/showCombineRes',
-        async:false,
-        data :{pid:pid},
-        //此处data为新建项目的pid
-        success : function (data) {
+        type:'POST',
+        url:'/personalTag/requestViewAllWork',
+        data:{username:uid,projectID:pid},
+        success:function(data){
             urls=data;
-            for(var i=0;i<urls.length;i++){
-                writePic(urls[i],i+1);
+            for (var i = 0; i < urls.length; i++) {
+                writePic(urls[i], i + 1);
+            }
+        },
+        error:function(data){
+            $.alert({
+                title: '预览工人完成结果失败!',
+                content: '由于不知名的原因，查看工人标注结果失败了',
+                confirmButton: '重新查看',
+                confirmButtonClass: 'btn-primary',
+                icon: 'fa fa-info',
+                animation: 'zoom'
+            });
+        }
+    });
+}else {
+    $.ajax({
+        type: 'POST',
+        url: '/project/showCombineRes',
+        async: false,
+        data: {pid: pid},
+        //此处data为新建项目的pid
+        success: function (data) {
+            urls = data;
+            for (var i = 0; i < urls.length; i++) {
+                writePic(urls[i], i + 1);
             }
         },
         error: function (data) {
             alert("新建任务出错")
         }
     })
+}
 initial();
 })
 
